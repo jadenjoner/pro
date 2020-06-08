a6f7e8dcac61cb8dd7c8a10b3169b1079c6f0754
Jaden Joner <jadenjoner@gmail.com>
2020-06-07 19:42:36 -0700

    added templates

---
+++ templates/programs/bash/main.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Hello there"
+
+echo -n "Dont say 'yes' : "
+read x
+$x
+
---
+++ templates/programs/python/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+import sys
+
+print( 'There are ' + str(len(sys.argv)) + ' arguments.' )
+print( 'Arguments: ' + str(sys.argv) )
